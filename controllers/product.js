5393ba3090f7e8b4e1bc9f8b61f2eb7c02b2f08c
Yves <yveskouadio111@yahoo.fr>
2021-10-15 09:34:05 +0000

    crud product

--- controllers/product.js
+++ controllers/product.js
@@ -8,7 +8,23 @@ const Product = require('../models/product')
 const {errorHandler} = require("../helpers/dbErrorHandler") 
 
 
+exports.productById = (req,res,next,id) => {
+    Product.findById(id).exec((err,product)=>{
+        if(err || !product){
+            return res.status(400).json({
+                error : "Produit non trouvé"
+            })
+        }
+        req.product = product
+        next()
+    })
+    
+}
 
+exports.read = (req,res) => {
+    req.product.photo = undefined
+    return res.json(req.product)
+}
 
 exports.create = (req,res) => {
     let form = new formidable.IncomingForm()
@@ -19,11 +35,24 @@ exports.create = (req,res) => {
                 error : "L'image ne peut être uploader"
             })
         }
-        // console.log('fields==',fields)
-        // console.log('files')
-        let product = new Product(fields)
 
+
+        //check for fields
+        const {name,description,price,category,quantity,shipping} = fields
+        
+        if(!name || !description || !price || !category || !quantity || !shipping){
+            return res.status(400).json({
+                error : "Tout les champs sont requis"
+            })
+        }
+
+        let product = new Product(fields)
         if(files.photo){
+            if(files.photo.size > 100000){
+                return res.status(400).json({
+                    error : "L'image est doit être inférieur a 1MB"
+                })
+            }
             product.photo.data = fs.readFileSync(files.photo.path)
             product.photo.contentType = files.photo.type
         }
@@ -39,3 +68,66 @@ exports.create = (req,res) => {
 
     })
 }
+
+exports.remove = (req,res) => {
+    let product = req.product
+
+    product.remove((err,deletedProduct)=>{
+        if(err){
+            return res.status(400).json({
+                err : errorHandler(err)
+            })
+        }
+        res.json({
+            "message": "Produit supprimé avec succès"
+        })
+    })
+}
+
+exports.update = (req,res) => {
+    let form = new formidable.IncomingForm()
+    form.keepExtensions = true
+    form.parse(req, (err,fields,files) => {
+        if(err){
+            return res.status(400).json({
+                error : "L'image ne peut être uploader"
+            })
+        }
+
+
+        //check for fields
+        const {name,description,price,category,quantity,shipping} = fields
+        
+        if(!name || !description || !price || !category || !quantity || !shipping){
+            return res.status(400).json({
+                error : "Tout les champs sont requis"
+            })
+        }
+
+        let product = req.product
+
+        product = _.extend(product,fields)
+
+        if(files.photo){
+            if(files.photo.size > 100000){
+                return res.status(400).json({
+                    error : "L'image est doit être inférieur a 1MB"
+                })
+            }
+            product.photo.data = fs.readFileSync(files.photo.path)
+            product.photo.contentType = files.photo.type
+        }
+
+        product.save((err,result)=>{
+            if(err){
+                return res.status(400).json({
+                    error : errorHandler(err)   
+                })
+            }
+            res.json({result})
+        })
+
+    })
+}
+
+
--- routes/product.js
+++ routes/product.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 
 //controllers
-const {create} = require('../controllers/product')
+const {create,productById,read,remove,update} = require('../controllers/product')
 
 //user middlewares
 const {isAdmin,isAuth,requireSignIn} = require('../controllers/auth')
@@ -11,9 +11,13 @@ const {isAdmin,isAuth,requireSignIn} = require('../controllers/auth')
 //get user id param
 const {userById} = require("../controllers/user")
 
+router.get('/product/:productId',read)
 router.post('/product/create/:userId', requireSignIn,isAuth, isAdmin , create)
+router.delete('/product/:productId/:userId', requireSignIn,isAuth,isAdmin,remove)
+router.put('/product/:productId/:userId', requireSignIn,isAuth,isAdmin,update)
 
 
 router.param("userId",userById)
+router.param("productId",productById)
 
 module.exports = router
